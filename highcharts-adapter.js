db3f51627e5dff35b03121cca8ea8a5a9922ca02
Mike Nelson <mdnelson30@gmail.com>
2010-05-05 14:38:09 -0400

    updates / commenting

--- highcharts-adapter.js
+++ highcharts-adapter.js
@@ -1,20 +1,27 @@
+// Written by Michael Nelson http://www.mikenelsons.com
+// Feel free to use / modify any of this
+
+// whoa dude, load prototype... you fresh meat?
 if(typeof Prototype == 'undefined')
 {
 	throw "Highcharts Adapter Error: Prototype has not been loaded.\n\tPlease load prototype prior to loading the highcharts adapter."
 }
 
+// Adapter interface between prototype and the highcarts charting library
 var HighchartsAdapter = {
 	
+	// el needs an event to be attached. el is not necessarily a dom element
 	addEvent: function (el, event, fn){
 		if($(el).observe)
 			Event.observe($(el), event, fn);
 		else {
 			HighchartsAdapter._extend(el);
-			el.observe_event(event, fn);
+			el._highcharts_observe(event, fn);
 		}
 	},
 	
-	animate: function (el, params, options) { // temporarily done
+	// motion makes things pretty. use it if effects is loaded, if not... still get to the end result.
+	animate: function (el, params, options) {
 		
 		var string = $H(params).collect(function(pair){ return [pair.key,pair.value].join(':');}).join(';');
 		if(options) $w('duration', 'delay').each(function(time){ options[time] = (!!(options[time]) ? options[time]/1000.0 : 0);});
@@ -24,10 +31,11 @@ var HighchartsAdapter = {
 			$(el).setStyle(string);
 	},
 	
-	each: function(arr, fn) { 
-		arr.each(fn);
-	},
+	// um.. each
+	each: function(arr, fn) { arr.each(fn); },
 	
+	// fire an event based on an event name (event) and an object (el).
+	// again, el may not be a dom element
 	fireEvent: function(el, event, eventArguments, defaultFunction) { 
 		if(event.preventDefault){
 			defaultFunction = null;
@@ -36,12 +44,14 @@ var HighchartsAdapter = {
 		if($(el).observe)
 			el.fire(event, eventArguments);
 		else if(el._highcharts_extended)
-			el.fire_event(event, eventArguments);
+			el._highcharts_fire(event, eventArguments);
 		
 		
 		if(defaultFunction) defaultFunction(event);
 	},
 	
+	// request data from <url>. for now, forcing json parsing even though this may change if highcharts uses other formats.
+	// execute callback with the data
 	getAjax: function (url, callback) { 
 		new Ajax.Request(url, {
 			method: 'get',
@@ -52,18 +62,18 @@ var HighchartsAdapter = {
 		});
 	},
 	
-	grep: function(arr, fn) { 
-		return arr.findAll(fn);
-	},
+	// um, grep
+	grep: function(arr, fn) { return arr.findAll(fn); },
 	
+	// change leftPadding to left-padding
 	hyphenate: function (str) { 
-		return str.replace(/([A-Z])/g, function(a, b){ return '-'+ b.toLowerCase() });
+		return str.replace(/([A-Z])/g, function(a,b){ return '-' + a.charAt(0).toLowerCase(); });
 	},
 	
-	map: function(arr, fn) { 
-		return arr.collect(fn);
-	},
+	// um, map
+	map: function(arr, fn) { return arr.map(fn); },
 	
+	// deep merge. merge({a : 'a', b : {b1 : 'b1', b2 : 'b2'}}, {b : {b2 : 'b2_prime'}, c : 'c'}) => {a : 'a', b : {b1 : 'b1', b2 : 'b2_prime'}, c : 'c'}
 	merge: function(){
 		function doCopy(copy, original) {
 			var value;
@@ -91,29 +101,36 @@ var HighchartsAdapter = {
 		return merge.apply(this, arguments);
 	},
 	
-	warn : function(text)
-	{
-		try{ console.warn(text); } catch(e){}
+	warn : function(text){
+		try{ console.warn(text); } catch(e) {}
+	},
+	
+	log : function(thing){
+		try{ console.log(thing); } catch(e) {}
+	},
+	
+	// extend an object to handle higchart events. 
+	// this is a very simple way of handling events but whatever, it works (i think)
+	_extend : function(object){
+		if(!object._highcharts_extended){
+			Object.extend(object, { _highchart_events : {}, _highcharts_extended : true,
+				_highcharts_observe : function(name, fn){
+					this._highchart_events[name] = [this._highchart_events[name], fn].compact().flatten();
+				},
+				_highcharts_fire : function(name, args){
+					(this._highchart_events[name] || []).each(function(fn){
+						if(args && args.stopped) 
+							return; // "throw $break" wasn't working. i think because of the scope of 'this'.
+						fn.bind(this)(args);
+					}.bind(this));
+				}
+			});
+		}
 	}
 };
 
-HighchartsAdapter._extend = function(object){
-	if(!object._highcharts_extended)
-		Object.extend(object, {
-			_highchart_events : {}, 
-			_highcharts_extended : true,
-			observe_event : function(name, fn){
-				this._highchart_events[name] = [this._highchart_events[name], fn].compact().flatten();
-			},
-			fire_event : function(name, args){
-				(this._highchart_events[name] || []).each(function(fn){
-					if(args && args.stopped) 
-						return; // "throw $break" wasn't working. i think because of the scope of 'this'.
-					fn.bind(this)(args);
-				}.bind(this));
-			}});
-};
 
+// warn about effects not being loaded
 if(typeof Effect == 'undefined')
 {
 	HighchartsAdapter.warn('effects.js was not loaded, animations will not occur.')
